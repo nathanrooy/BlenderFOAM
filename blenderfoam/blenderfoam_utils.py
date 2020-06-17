0eb2dee09101e6276c20e87739dbaa6c1d1edc74
nathanrooy <nathanrooy@gmail.com>
2020-06-17 12:26:18 -0400

    update

--- blenderfoam/blenderfoam_utils.py
+++ blenderfoam/blenderfoam_utils.py
@@ -1,29 +1,25 @@
-def initialize_optimizer(configs):
-	
-
-	
-
-	return 
+from scipy.optimize import minimize
 
 
 def run_case(configs):
 
+	# load initial values
+	x0 = 'something'
+
 	# initialize optimizer
+	res = minimize(cost_func, x0, method=configs['optimizer']['name'])
 	
-	# run
 	
 	return
 	
 		
-		
-	
 def cost_function(configs):
 
 	# apply mesh deformation and save new geometry
 	
 	# prepare new openfoam iteration
 
-	# run openfoam (if nesessary)
+	# run openfoam
 	
 	# collect results
 	
---
+++ tutorials/2d_airfoil_shape_match/cost.py
@@ -0,0 +1,14 @@
+def calc_mse(path_latest):
+	'''Get the mean-squared error between two airfoils
+	'''
+	
+	# path to initial case
+	path_initial = 'initial/'
+	
+	# load airfoil vertices from initial case
+	
+	# load airfoil vertices from latest case
+	
+	# calculate mse
+	
+	return mse
